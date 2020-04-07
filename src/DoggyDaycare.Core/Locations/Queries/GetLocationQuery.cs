46e6359db937915702c2d44f1de2084e243b0bb5
Josiah Mortenson <josiah.mortenson@gmail.com>
2020-04-06 23:16:37 -0500

    added get location query with mock repository

---
+++ src/DoggyDaycare.Core/Locations/Queries/GetLocationQuery.cs
@@ -0,0 +1,31 @@
+﻿using DoggyDaycare.Core.Common;
+using DoggyDaycare.Core.Locations.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DoggyDaycare.Core.Locations.Queries
+{
+    public class GetLocationQuery : IRequest<Location>
+    {
+        public string Id { get; set; }
+    }
+
+    public class GetLocationQueryHandler : IRequestHandler<GetLocationQuery, Location>
+    {
+        private readonly ILocationRepository _repository;
+
+        public GetLocationQueryHandler(ILocationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Location> Handle(GetLocationQuery request, CancellationToken cancellationToken)
+        {
+            return _repository.Find(request.Id);
+        }
+    }
+}
---
+++ tests/DoggyDaycare.Core.Tests/Locations/Queries/GetLocationQueryTest.cs
@@ -0,0 +1,65 @@
+﻿using DoggyDaycare.Core.Common;
+using DoggyDaycare.Core.Locations.Entities;
+using DoggyDaycare.Core.Locations.Queries;
+using DoggyDaycare.Infrastructure;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DoggyDaycare.Core.Tests.Locations.Queries
+{
+    public class GetLocationQueryTest
+    {
+        private readonly IMediator _mediator;
+
+        public GetLocationQueryTest()
+        {
+            var services = new ServiceCollection();
+            services.AddMediatR(typeof(GetLocationQuery));
+            services.AddScoped<ILocationRepository, MockLocationRepository>();
+            _mediator = services.BuildServiceProvider().GetService<IMediator>();
+        }
+
+        [Fact]
+        public async void ShouldReturnLocation()
+        {
+            // Arrange
+            var expected = new Location
+            {
+                Id = "1",
+                Name = "South Store"
+            };
+            var query = new GetLocationQuery
+            {
+                Id = "1"
+            };
+
+            // Act
+            var location = await _mediator.Send(query);
+
+            // Assert
+            Assert.Equal(expected.Id, location.Id);
+            Assert.Equal(expected.Name, location.Name);
+
+        }
+
+        [Fact]
+        public async void ShouldReturnNullLocation()
+        {
+            // Arrange
+            var query = new GetLocationQuery
+            {
+                Id = "-1"
+            };
+
+            // Act
+            var location = await _mediator.Send(query);
+
+            // Assert
+            Assert.Null(location);
+        }
+    }
+}
