9d79f665b41d49cbf3bddcf7319deedc30b7b77e
Josiah Mortenson <josiah.mortenson@gmail.com>
2020-04-06 22:35:27 -0500

    added get organization query

---
+++ src/DoggyDaycare.Core/Organizations/Queries/GetOrganizationQuery.cs
@@ -0,0 +1,32 @@
+﻿using DoggyDaycare.Core.Common;
+using DoggyDaycare.Core.Organizations.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DoggyDaycare.Core.Organizations.Queries
+{
+    public class GetOrganizationQuery : IRequest<Organization>
+    {
+        public string Id { get; set; }
+    }
+
+    public class GetOrganizationQueryHandler : IRequestHandler<GetOrganizationQuery, Organization>
+    {
+        private readonly IOrganizationRepository _repostiory;
+
+        public GetOrganizationQueryHandler(IOrganizationRepository repository)
+        {
+            _repostiory = repository;
+        }
+
+        public async Task<Organization> Handle(GetOrganizationQuery request, CancellationToken cancellationToken)
+        {
+            var organization = _repostiory.Find(request.Id);
+            return organization;
+        }
+    }
+}
---
+++ tests/DoggyDaycare.Core.Tests/Organizations/Queries/GetOrganizationQueryTest.cs
@@ -0,0 +1,50 @@
+﻿using DoggyDaycare.Core.Common;
+using DoggyDaycare.Core.Organizations.Entities;
+using DoggyDaycare.Core.Organizations.Queries;
+using DoggyDaycare.Infrastructure;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DoggyDaycare.Core.Tests.Organizations.Queries
+{
+    public class GetOrganizationQueryTest
+    {
+        private readonly IMediator _mediator;
+
+        public GetOrganizationQueryTest()
+        {
+            var services = new ServiceCollection();
+            services.AddMediatR(typeof(GetOrganizationQuery));
+            services.AddSingleton<IOrganizationRepository, MockOrganizationRepository>();
+            var servicesProvider = services.BuildServiceProvider();
+            _mediator = servicesProvider.GetService<IMediator>();
+        }
+
+        [Fact]
+        public async void ShouldGetOrganization()
+        {
+            // Arrange
+            var expected = new Organization
+            {
+                Id = "1",
+                Name = "DoggyDaycare"
+            };
+
+            var query = new GetOrganizationQuery
+            {
+                Id = expected.Id
+            };
+
+            // Act
+            var result = await _mediator.Send(query);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(result.Name, expected.Name);
+        }
+    }
+}
