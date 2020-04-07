94eb68ccc5d9a98cbd1c81afc7fb5f87fccda1a9
Josiah Mortenson <josiah.mortenson@gmail.com>
2020-04-06 21:45:49 -0500

    added update pet command

---
+++ src/DoggyDaycare.Core/Pets/Commands/UpdatePetCommand.cs
@@ -0,0 +1,31 @@
+﻿using DoggyDaycare.Core.Common;
+using DoggyDaycare.Core.Pets.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DoggyDaycare.Core.Pets.Commands
+{
+    public class UpdatePetCommand : IRequest<Pet>
+    {
+        public Pet Pet { get; set; }
+    }
+
+    public class UpdatePetCommandHandler : IRequestHandler<UpdatePetCommand, Pet>
+    {
+        private readonly IPetRepository _repository;
+        public UpdatePetCommandHandler(IPetRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Pet> Handle(UpdatePetCommand request, CancellationToken cancellationToken)
+        {
+            var pet = _repository.Update(request.Pet);
+            return pet;
+        }
+    }
+}
---
+++ tests/DoggyDaycare.Core.Tests/Pets/Commands/UpdatePetCommandTest.cs
@@ -0,0 +1,51 @@
+﻿using DoggyDaycare.Core.Common;
+using DoggyDaycare.Core.Pets.Commands;
+using DoggyDaycare.Core.Pets.Entities;
+using DoggyDaycare.Infrastructure;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DoggyDaycare.Core.Tests.Pets.Commands
+{
+    public class UpdatePetCommandTest
+    {
+        private readonly IMediator _mediator;
+        private readonly IPetRepository _repository;
+        public UpdatePetCommandTest()
+        {
+            var services = new ServiceCollection();
+            services.AddMediatR(typeof(UpdatePetCommand));
+            services.AddScoped<IPetRepository, MockPetRepository>();
+            var servicesProvider = services.BuildServiceProvider();
+            _mediator = servicesProvider.GetService<IMediator>();
+            _repository = servicesProvider.GetService<IPetRepository>();
+        }
+
+        [Fact]
+        public async void ShouldUpdatePet()
+        {
+            // Arrange
+            var updatedPet = new Pet
+            {
+                Id = "1",
+                Name = "Pickles"
+            };
+            var command = new UpdatePetCommand
+            {
+                Pet = updatedPet
+            };
+
+            // Act
+            var pet = await _mediator.Send(command);
+
+            // Assert
+            var result = _repository.Find(updatedPet.Id);
+            Assert.NotNull(pet);
+            Assert.Equal(updatedPet.Name, result.Name);
+        }
+    }
+}
